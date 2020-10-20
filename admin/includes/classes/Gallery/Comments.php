a1b7eb9e7544fb41e132b5cab01d9b2044149d9a
Eli <elit1989+1@gmail.com>
2020-10-20 23:03:05 +0300

    added wysiwyg editor for photos and minor refctoring

---
+++ admin/includes/classes/Gallery/Comments.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Gallery;
+
+
+class Comments extends Database
+{
+    protected string $table = 'comments';
+    protected $fillables = [];
+}
--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -6,7 +6,7 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-  <script src="https://cdn.tiny.cloud/1/eyqcayy3xmu4dknrpzz094xpsgaid3sol67xi1yvxqbxigjq/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+  <script src="https://cdn.tiny.cloud/1/<?php echo $wysiwygKey?>/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 </body>
 
 </html>
