32931be09686a3ac709ec34bc392ac22fac18759
Eli <elit1989+1@gmail.com>
2021-02-11 23:56:07 +0200

    code styling changes

--- admin/includes/classes/Gallery/Utils.php
+++ admin/includes/classes/Gallery/Utils.php
@@ -6,13 +6,11 @@ namespace Gallery;
 
 use finfo;
 
-class Utils
-{
+class Utils {
     /**
      * @param string $location
      */
-    public static function redirect(string $location): void
-    {
+    public static function redirect(string $location): void {
         header("Location: $location");
     }
 
@@ -22,8 +20,7 @@ class Utils
      * @param array $data
      * All messages from back-end processed here.
      */
-    public static function sendFinalResponseAsJson(bool $success = true, string $message = '', $data = []): void
-    {
+    public static function sendFinalResponseAsJson(bool $success = true, string $message = '', $data = []): void {
         if (!is_array($data)) {
             $data = [$data];
         }
@@ -47,12 +44,14 @@ class Utils
                 mkdir($pathname);
             }
 
-            $fileExtension  = pathinfo($uploadedFile['name'], PATHINFO_EXTENSION);
-            $fileName       = sprintf('%s.%s', sha1_file(Photos::$tmp_path), $fileExtension);
+            $fileExtension = pathinfo($uploadedFile['name'], PATHINFO_EXTENSION);
+            $fileName = sprintf('%s.%s', sha1_file(Photos::$tmp_path), $fileExtension);
             $_POST['image'] = $userAvatar ? USER_AVATARS_FILE_URL . $fileName : $fileName;
-            $targetPath     = "{$pathname}" . $fileName;
+            $targetPath = "{$pathname}" . $fileName;
+
             return move_uploaded_file(Photos::$tmp_path, $targetPath);
         }
+
         return false;
     }
 
@@ -62,14 +61,14 @@ class Utils
      */
     private static function validateProvidedFile($uploadedFile): bool {
         Photos::$tmp_path = $uploadedFile["tmp_name"];
-        $fileInfo         = new finfo(FILEINFO_MIME_TYPE);
-        $fileExtension    = $fileInfo->file(Photos::$tmp_path);
+        $fileInfo = new finfo(FILEINFO_MIME_TYPE);
+        $fileExtension = $fileInfo->file(Photos::$tmp_path);
 
-        $fileExists       = boolval(file_exists(Photos::$tmp_path));
-        $allowedFileType  = in_array($fileExtension, ALLOWED_MIME_TYPES);
-        $allowedSize      = boolval($uploadedFile['size'] < MAX_ALLOWED_FILE_SIZE);
+        $fileExists = boolval(file_exists(Photos::$tmp_path));
+        $allowedFileType = in_array($fileExtension, ALLOWED_MIME_TYPES);
+        $allowedSize = boolval($uploadedFile['size'] < MAX_ALLOWED_FILE_SIZE);
         $notMultipleFiles = boolval(!isset($uploadedFile['error']) || !is_array($uploadedFile['error']));
-        $noErrors         = ($uploadedFile['error'] === UPLOAD_ERR_OK);
+        $noErrors = ($uploadedFile['error'] === UPLOAD_ERR_OK);
 
         return ($fileExists && $allowedFileType && $allowedSize && $notMultipleFiles && $noErrors);
     }
